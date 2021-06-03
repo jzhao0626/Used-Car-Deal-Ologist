8fc51b82033818f8812ae03576e64d56c59d5a96
Sebastian Echeverry <uno.sebastian.echo@gmail.com>
2021-06-03 16:52:29 -0400

    training-grounds

---
+++ mapping_values.py
@@ -0,0 +1,43 @@
+vehicle_condition_map = {
+    "new": 1,
+    "like new": 2,
+    "excellent": 3,
+    "good": 4,
+    "fair": 5,
+    "salvage": 6,
+}
+
+vehicle_fuel_type_map = {
+    "electric": 1,
+    "hybrid": 2,
+    "gas": 3,
+    "diesel": 4,
+    "other": 5,
+}
+
+vehicle_title_status_map = {
+    "clean": 1,
+    "rebuilt": 2,
+    "lien": 3,
+    "salvage": 4,
+    "parts only": 5,
+    "missing": 6,
+}
+
+vehicle_transmission_map = {
+    "automatic": 1,
+    "other": 2,
+    "manual": 3,
+}
+
+vehicle_size_map = {
+    "compact": 1,
+    "sub-compact": 2,
+    "mid-size": 3,
+    "full-size": 4,
+}
+
+
+# manufacturer = 'bmw','toyota','honda','chevrolet','mazda','ford','volvo','cadillac','saturn','subaru','dodge','gmc','ram','chrysler','mercedes-benz','infiniti','jeep','buick','nissan','volkswagen','mercury','hyundai','lexus','porsche','rover','audi','fiat','mini','mitsubishi','lincoln','jaguar','kia','pontiac','acura','tesla','alfa-romeo','datsun','harley-davidson','land rover','aston-martin','ferrari'
+# type = 'SUV','mini-van','convertible','coupe','truck','wagon','sedan','pickup','hatchback','van','other','bus','offroad'
+# paint_color = 'blue','white','grey','black','brown','red','silver','green','yellow','purple','custom','orange'
